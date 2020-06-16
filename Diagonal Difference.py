843ebac41b997897cce20e135b746c90b663706d
Ashik <aharonno2017@gmail.com>
2020-06-16 19:32:22 +0600

    Basic problem

---
+++ Diagonal Difference.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr,n):
+    sum=0
+    sum1=0
+    sum2=0
+    for i in range(len(arr)):
+        sum1+=arr[i][i]
+        sum2+=arr[i][n-i-1]
+    return abs(sum1-sum2)
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr,n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
